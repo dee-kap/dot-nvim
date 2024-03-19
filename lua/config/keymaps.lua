a261e50db8cf53e33aa91120206719e8dbd317fc
deepak <dee-kap@users.noreply.github.com>
2024-03-19 20:02:36 +1100

    Copilot complete

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,3 +21,9 @@ end, {})
 vim.api.nvim_set_keymap("n", "<leader>ce", ":RunFile<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true
